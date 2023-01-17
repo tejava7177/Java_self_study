d04835d7c2836c3a545eac7415cf1f218f4772b4
Sim-JuHeun <88835746+tejava7177@users.noreply.github.com>
2023-01-17 22:22:40 +0900

    Add files via upload

---
+++ scope/ScopeDemo8.java
@@ -0,0 +1,20 @@
+package org.opentutorials.javatutorials.scope;
+ 
+class C3 {
+    int v = 10;
+ 
+    void m() {
+        int v = 20;
+        System.out.println(v);
+        System.out.println(this.v);
+    }
+}
+ 
+public class ScopeDemo8 {
+ 
+    public static void main(String[] args) {
+        C3 c1 = new C3();
+        c1.m();
+    }
+ 
+}
